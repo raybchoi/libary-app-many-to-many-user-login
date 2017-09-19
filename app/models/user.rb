951c0ff19eb2ebbf103f8fbf93b225822e00e4d1
raybchoi <raybchoi@gmail.com>
2017-09-19 14:58:55 -0700

    user form created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  # this takes the password upon user.create and from the hash it looks for the key password and then it hits this line called 'has_secure_password'. This then calls in bcrypt to turn it into an encrypted password to save it
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # adds a route
   root to: "users#index"
+
+  # main page
+  get '/users' => "users#index"
+  # shows the new form
+  get "/users/new" => "users#new", as:"new_user"
+  # creates the user
+  post "/users" => "users#create"
+  # shows one user
+  get "/users/:id" => "users#show", as: "user"
+
+  # Logging in and logging out is controlled by the sessions controller.
+  get "/login" => "sessions#new"
+
 end
+# Self-check for understanding: Justify the choice to use sessions#new for the log in form. => you have a new user so you want them to create a new session form
+
+# Self-check for understanding: Which sessions controller action will we use to actually set the session and log the user in? => post '/login', to: 'sessions#create'
+
+# Self-check for understanding: Which sessions controller action will we use to log out? => post '/logout', to: 'sessions#destroy'
