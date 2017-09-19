02883b19aa694eff77a185a35ab35cdc6c352d24
raybchoi <raybchoi@gmail.com>
2017-09-19 12:03:20 -0700

    created user model

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # adds a route
+  root to: "users#index"
+end
